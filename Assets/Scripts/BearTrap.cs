e10aa71ebd1413000d7ee4cf085c42b43c6ddcc2
Becca Little <beccannlittle@gmail.com>
2018-11-11 15:45:55 -0700

    reorg and moving away from scroller paradigm

---
+++ Assets/Scripts/BearTrap.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BearTrap : MonoBehaviour {
+
+    private Animator anim;
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            anim.SetTrigger("Trapped");
+            var player = other.gameObject.transform.parent.GetComponent<Player>();
+            gameObject.transform.position = player.groundCheck.position;
+            gameObject.transform.rotation = player.groundCheck.rotation;
+            player.Die();
+        }
+    }
+}
---
+++ Assets/Scripts/Shia.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shia : MonoBehaviour {
+
+    public float speed = 5f;
+    public Player player;
+
+    private Rigidbody2D rb { get; set; }
+    private Animator anim;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.freezeRotation = true;
+        anim = GetComponentInChildren<Animator>();
+        player.playerDied += DoVictory;
+    }
+
+    private bool isStopped;
+
+    private void FixedUpdate()
+    {
+        if (!isStopped)
+        {
+            rb.velocity = new Vector2(speed, rb.velocity.y);
+        }
+    }
+
+    private void DoVictory()
+    {
+        isStopped = true;
+        rb.velocity = new Vector2(0f, 0f);
+        anim.SetTrigger("Stopped");
+    }
+}
